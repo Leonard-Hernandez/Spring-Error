19c8bb6ec9eb7f13f80179d2ae4d21c5ef7dc311
Leonard David Hernandez Parra <leonardhernandez02@gmail.com>
2024-09-27 11:53:57 -0500

    Manejando la excepcion not found

--- src/main/java/com/leonard/springboot/error/springboot_demo/controllers/HandlerExceptionController.java
+++ src/main/java/com/leonard/springboot/error/springboot_demo/controllers/HandlerExceptionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.leonard.springboot.error.springboot_demo.models.Error;
 
@@ -19,9 +20,18 @@ public class HandlerExceptionController {
                 "Error divicion 0",
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 new Date());
-                
+
         //return ResponseEntity.internalServerError().body(error);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFoundex(NoHandlerFoundException ex){
+
+        Error error = new Error(ex.getMessage(), "Api no encontrada", HttpStatus.NOT_FOUND.value(), new Date());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
+
     }
 
 }

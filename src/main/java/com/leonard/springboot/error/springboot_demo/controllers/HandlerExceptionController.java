933fc34f19fe3fe12d9ff018eaf9b0ae88053453
Leonard David Hernandez Parra <leonardhernandez02@gmail.com>
2024-09-27 12:08:56 -0500

    Mapeando el error con un Map y personalizando en codigo de salida con @ResponseStatus

--- src/main/java/com/leonard/springboot/error/springboot_demo/controllers/HandlerExceptionController.java
+++ src/main/java/com/leonard/springboot/error/springboot_demo/controllers/HandlerExceptionController.java
@@ -1,10 +1,13 @@
 package com.leonard.springboot.error.springboot_demo.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -21,16 +24,33 @@ public class HandlerExceptionController {
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 new Date());
 
-        //return ResponseEntity.internalServerError().body(error);
+        // return ResponseEntity.internalServerError().body(error);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<Error> notFoundex(NoHandlerFoundException ex){
+    public ResponseEntity<Error> notFoundex(NoHandlerFoundException ex) {
 
-        Error error = new Error(ex.getMessage(), "Api no encontrada", HttpStatus.NOT_FOUND.value(), new Date());
+        Error error = new Error(ex.getMessage(),
+                "Api no encontrada",
+                HttpStatus.NOT_FOUND.value(),
+                new Date());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
+
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> numberFormatException(NumberFormatException ex) {
+
+        Map<String, String> error = new HashMap<>();
+        error.put("date", new Date().toString());
+        error.put("message", ex.getMessage());
+        error.put("Error", "Fomarmato de numero no valido");
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value() + "");
+
+        return error;
 
     }
 

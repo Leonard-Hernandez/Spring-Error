1d8cf69f65e96bc287b38fdc76d54bf516a2ad2b
Leonard <Leonardhernandez02@gmail.com>
2025-02-01 23:52:15 -0500

    adelanto

---
+++ src/main/java/com/leonard/springboot/error/AppConfig.java
@@ -0,0 +1,26 @@
+package com.leonard.springboot.error;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.leonard.springboot.error.springboot_demo.models.domain.User;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    List<User> users() {
+        List<User> users = new ArrayList<>();
+        users.add(new User(1L, "Leonard", "Hernandez"));
+        users.add(new User(2L, "Lubraska", "Ferreira"));
+        users.add(new User(3L, "Andres", "Guzman"));
+        users.add(new User(4L, "Carlos", "gomez"));
+        users.add(new User(5L, "Jose", "Gutierrez"));
+
+        return users;
+    }
+
+}
--- src/main/java/com/leonard/springboot/error/springboot_demo/services/UserServiceImpl.java
+++ src/main/java/com/leonard/springboot/error/springboot_demo/services/UserServiceImpl.java
@@ -1,26 +1,21 @@
 package com.leonard.springboot.error.springboot_demo.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.leonard.springboot.error.springboot_demo.exceptions.UserNotFoundException;
 import com.leonard.springboot.error.springboot_demo.models.domain.User;
 
 @Service
 public class UserServiceImpl implements UserService {
-
+    
     private List<User> users;
 
-    public UserServiceImpl() {
-        this.users = new ArrayList<>();
-        users.add(new User(1L, "Leonard", "Hernandez"));
-        users.add(new User(2L, "Lubraska", "Ferreira"));
-        users.add(new User(3L, "Andres", "Guzman"));
-        users.add(new User(4L, "Carlos", "gomez"));
-        users.add(new User(5L, "Jose", "Gutierrez"));
+    @Autowired
+    public void setUsers(List<User> users) {
+        this.users = users;
     }
 
     @Override
@@ -40,5 +35,4 @@ public class UserServiceImpl implements UserService {
         System.out.println(user.getName());
         return Optional.ofNullable(user);
     }
-
 }
